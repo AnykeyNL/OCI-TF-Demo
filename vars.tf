50f928dde296b7ea8e98ca5a7c40d75267e94dee
richard <the.anykey@gmail.com>
2020-12-24 09:57:50 +0100

    Basic Networking + VM

--- vars.tf
+++ vars.tf
@@ -1,4 +1,4 @@
-variable compartment_ocid { }
-variable region { }
-variable vcn_name { }
+variable compartment_ocid { default="ocid1.compartment.oc1..aaaaaaaadqqs3u2movchi7ebiyfm7gnvv6lbnuey4x2iawothkt24et4kneq" }
+variable region { default = "eu-frankfurt-1"}
+variable vcn_name { default = "MyVCN"}
 
